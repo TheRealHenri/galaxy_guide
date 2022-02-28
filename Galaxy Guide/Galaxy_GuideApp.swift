d41139bb1774c28ecd18fee488a911697c4fcbf5
Henri Allgöwer <h.allg2@gmx.de>
2022-02-28 15:28:22 +0100

    initial commit

---
+++ Galaxy Guide/Galaxy_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Galaxy_GuideApp.swift
+//  Galaxy Guide
+//
+//  Created by Henri Allgöwer on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct Galaxy_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
